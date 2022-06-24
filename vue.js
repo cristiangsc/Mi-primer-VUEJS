490cac7602c4551552854299afcf2d17933edc28
Cristian Sepulveda <cristiangsc@gmail.com>
2022-06-23 20:54:39 -0400

    Mi primer VUE JS

---
+++ vue.js
@@ -0,0 +1,27 @@
+let app = new Vue({
+    el: "#app",
+    data: {
+        usuarios: [{
+                nombre: "Jose",
+                apellido: "Pérez",
+                run: "14472607-2",
+                nacimiento: "12-04-1984",
+                edad: 38,
+            },
+            {
+                nombre: "María",
+                apellido: "rodríguez",
+                run: "14445407-3",
+                nacimiento: "12-09-1980",
+                edad: 42,
+            },
+            {
+                nombre: "Manuel",
+                apellido: "Patiño",
+                run: "24472600-1",
+                nacimiento: "10-05-1988",
+                edad: 34,
+            },
+        ],
+    },
+});
